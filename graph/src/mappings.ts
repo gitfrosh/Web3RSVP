89849f39a2d7995337c9b5b3ede71d1cb0f07735
rike <hello@rike.dev>
2022-09-02 21:00:34 +0200

    log

--- graph/src/mappings.ts
+++ graph/src/mappings.ts
@@ -25,6 +25,7 @@ export function handleNewEventCreated(event: NewEventCreated): void {
 
     if (metadata) {
       const value = json.fromBytes(metadata).toObject();
+      console.log(value);
       if (value) {
         const name = value.get("name");
         const description = value.get("description");
